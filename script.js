3e8a2868722ebb6c13f00f160cd45b2f09a2ad31
Aftab-dev-personal <aftab.dev4@gmail.com>
2024-09-27 02:05:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you next day!!!!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  question.innerHTML = "Yay, see you tomorrowwwwwwwwwwwwww!!!!";
+  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
